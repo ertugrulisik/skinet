9f4b85c8c77a3e0f066f5e8b6fe27254f8af3841
Ertugrul ISIK <ertugrulisik@Ertugruls-MacBook-Pro.local>
2020-10-26 08:01:21 +0300

    added ProductConfiguration class and specified cloumns' properties when database is creating

---
+++ Infrastructure/Data/Config/ProductConfiguration.cs
@@ -0,0 +1,23 @@
+using System.Security.Cryptography.X509Certificates;
+using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Data.Config
+{
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.Property(x => x.Id).IsRequired();
+            builder.Property(x => x.Name).IsRequired().HasMaxLength(250);
+            builder.Property(x => x.Description).IsRequired().HasMaxLength(500);
+            builder.Property(x => x.Price).HasColumnType("decimal(18,2)");
+            builder.Property(x => x.PictureUrl).IsRequired();
+            builder.HasOne(b => b.ProductBrand).WithMany()
+                .HasForeignKey(p => p.ProductBrandId);
+            builder.HasOne(t => t.ProductType).WithMany()
+                .HasForeignKey(p => p.ProductTypeId);
+        }
+    }
+}
--- Infrastructure/Data/Migrations/StoreContextModelSnapshot.cs
+++ Infrastructure/Data/Migrations/StoreContextModelSnapshot.cs
@@ -22,16 +22,21 @@ namespace Infrastructure.Data.Migrations
                         .HasColumnType("INTEGER");
 
                     b.Property<string>("Description")
-                        .HasColumnType("TEXT");
+                        .IsRequired()
+                        .HasColumnType("TEXT")
+                        .HasMaxLength(500);
 
                     b.Property<string>("Name")
-                        .HasColumnType("TEXT");
+                        .IsRequired()
+                        .HasColumnType("TEXT")
+                        .HasMaxLength(250);
 
                     b.Property<string>("PictureUrl")
+                        .IsRequired()
                         .HasColumnType("TEXT");
 
                     b.Property<decimal>("Price")
-                        .HasColumnType("TEXT");
+                        .HasColumnType("decimal(18,2)");
 
                     b.Property<int>("ProductBrandId")
                         .HasColumnType("INTEGER");
